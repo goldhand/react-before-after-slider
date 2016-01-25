50c8fddc8e8147ea2487a331bd18594526293a2e
Will Farley <willf@hzdg.com>
2016-01-25 18:47:55 -0500

    update mkcomponent script

--- README.md
+++ README.md
@@ -39,8 +39,34 @@ Commands
 
 To make build a new component using with boilerplate run:
 
+    $ npm run mkcomponent ComponentName -- [-t redux]
 
-    $ npm run mkcomponent ComponentName
+or
+
+    $ npm run mkcomponentredux ComponentName
 
 this will create a new component dir in `./src/components/` and add component.js,
 component.less and package.json files.
+
+You can change the default new component boilerplate by editing `./bin/mkComponent.sh`
+there are two templates in that file, a plain react component and a redux react component
+
+
+Deployment
+----------
+
+To use the development deployment commands edit `package.json` to reflect the projects development server and path to the releases directory. Add the keys `devServer` and `devServerPath` like so:
+
+    "devServer": "mercury.hzdesign.com",
+    "devServerPath": "/srv/http/webpack-hzskeleton.mercury.hzdesign.com/releases/",
+
+make sure the folder exists on the server and run
+
+    $ npm run deploydev
+
+to deploy to the development server
+
+to undo the last deployment run
+
+    $ npm run undodeploydev
+
--- bin/mkComponent.sh
+++ bin/mkComponent.sh
@@ -1,11 +1,42 @@
 #!/bin/sh
+usage="$(basename "$0") <component_name> [-t templatename] [-h] -- creates a new react component
 
-# makes a new react component
-APP_NAME=$1;
+where
+    -h  show this help text
+    -t  template"
+
+APP_NAME=$1
+OPTIND=1
 if [ "$COMPONENTS_DIR" == "" ]; then
     COMPONENTS_DIR='./src/components';
 fi;
-if [ "$APP_NAME" == "" ]; then
+if [[ $APP_NAME != \-* ]]; then
+  OPTIND=2
+fi;
+
+while getopts ":t:h" opt; do
+  case $opt in
+    h)
+      echo "$usage" >&2
+      exit
+      ;;
+    t)
+      TEMPLATE=$OPTARG
+      echo "Using $OPTARG template\n" >&2
+      ;;
+    :)
+      echo "Need a valid template\n" >&2
+      echo "$usage" >&2
+      ;;
+    \?)
+      printf "Invalid option -%s\n" "$OPTARG" >&2
+      echo "$usage" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ $APP_NAME == \-* ]] || [[ ! $APP_NAME ]]; then
     echo "App Name? ";
     read APP_NAME;
 fi;
@@ -24,7 +55,7 @@ read -d '' STYLE_FILE <<- EOF
 @import '../base.less';
 EOF
 
-read -d '' COMPONENT_FILE <<- EOF
+read -d '' REDUX_COMPONENT_FILE <<- EOF
 import React from 'react';
 import './$APP_NAME.less';
 
@@ -33,17 +64,20 @@ const {PropTypes} = React;
 
 export default class $APP_NAME extends React.Component {
 
+  static propTypes = {
+  };
+
   static contextTypes = {
     store: PropTypes.object,
-  }
+  };
   componentDidMount() {
     this.unsubscribe = this.context.store.subscribe(() => {
       this.forceUpdate();
     });
-  }
+  };
   componentWillUnmount() {
     this.unsubscribe();
-  }
+  };
 
   render = () => {
     const {store} = this.context,
@@ -51,8 +85,29 @@ export default class $APP_NAME extends React.Component {
 
     return (
 
-    )
-  }
+    );
+  };
+}
+EOF
+
+
+read -d '' REACT_COMPONENT_FILE <<- EOF
+import React from 'react';
+import './$APP_NAME.less';
+
+
+const {PropTypes} = React;
+
+export default class $APP_NAME extends React.Component {
+
+  static propTypes = {
+  };
+
+  render = () => {
+    return (
+
+    );
+  };
 }
 EOF
 
@@ -60,7 +115,12 @@ main() {
     mkdir "$COMPONENTS_DIR/$APP_NAME";
     echo "$PACKAGE_FILE" >> "$COMPONENTS_DIR/$APP_NAME/package.json";
     echo "$STYLE_FILE" >> "$COMPONENTS_DIR/$APP_NAME/$APP_NAME.less";
-    echo "$COMPONENT_FILE" >> "$COMPONENTS_DIR/$APP_NAME/$APP_NAME.js";
+    if [ "$TEMPLATE" == "" ]; then
+      echo "$REACT_COMPONENT_FILE" >> "$COMPONENTS_DIR/$APP_NAME/$APP_NAME.js";
+    fi
+    if [ "$TEMPLATE" == "redux" ]; then
+      echo "$REDUX_COMPONENT_FILE" >> "$COMPONENTS_DIR/$APP_NAME/$APP_NAME.js";
+    fi
 }
 
 if [ "$APP_NAME" != "" ]; then
